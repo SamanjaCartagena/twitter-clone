174aeb14eafc040766b986b5309e0b52b10c7344
Samanja Cartagena <70960407+SamanjaCartagena@users.noreply.github.com>
2021-04-10 10:40:17 -0400

    waiting for firebase to stop buug

--- src/Feed.js
+++ src/Feed.js
@@ -1,29 +1,39 @@
-import React from 'react'
-import './Feed.css';
-import TweetBox from './TweetBox';
-import Post from './Post';
-function Feed() {
-    return (
-        <div className="feed">
-            <div className="feed__header">
-            <h2>Home</h2>
-            </div>
-            <TweetBox/>
-            <Post displayName="Sonny Sangha"
-            username='sssangha' verified='true'
-            text='Yoo its working'
-            avatar="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png"
-            image="https://cdn.pixabay.com/photo/2015/04/19/08/33/flower-729512__340.jpg"
-            
-            
-            />
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
+import React, { useState, useEffect } from "react";
+import TweetBox from "./TweetBox";
+import Post from "./Post";
+import "./Feed.css";
+import db from "./firebase";
 
-        </div>
-    )
+function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapshot) =>
+      setPosts(snapshot.docs.map((doc) => doc.data()))
+    );
+  }, []);
+
+  return (
+    <div className="feed">
+      <div className="feed__header">
+        <h2>Home</h2>
+      </div>
+
+      <TweetBox />
+
+        {posts.map((post) => (
+          <Post
+            key={post.text}
+            displayName={post.displayName}
+            username={post.username}
+            verified={post.verified}
+            text={post.text}
+            avatar={post.avatar}
+            image={post.image}
+          />
+        ))}
+    </div>
+  );
 }
 
-export default Feed
+export default Feed;
--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,17 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import firebase from 'firebase';
 
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyChs2UzI638NyjyR6wkWlAyi4c3AbwulbU",
-    authDomain: "blogx-3615d.firebaseapp.com",
-    databaseURL: "https://blogx-3615d.firebaseio.com",
-    projectId: "blogx-3615d",
-    storageBucket: "blogx-3615d.appspot.com",
-    messagingSenderId: "720982853163",
-    appId: "1:720982853163:web:7755fdce2e22b56963cc6e",
-    measurementId: "G-H233CJGBZ7"
-  };
+  apiKey: "AIzaSyChs2UzI638NyjyR6wkWlAyi4c3AbwulbU",
+  authDomain: "blogx-3615d.firebaseapp.com",
+  databaseURL: "https://blogx-3615d.firebaseio.com",
+  projectId: "blogx-3615d",
+  storageBucket: "blogx-3615d.appspot.com",
+  messagingSenderId: "720982853163",
+  appId: "1:720982853163:web:7755fdce2e22b56963cc6e",
+  measurementId: "G-H233CJGBZ7"
+};
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const db = firebaseApp.firestore();
 
